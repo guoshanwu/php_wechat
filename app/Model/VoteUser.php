7b450b2c6880725228eadfbd9b32cb91afe2c985
linjianqun <11038667@qq.com>
2019-06-13 19:48:30 +0800

    ...

--- app/Model/VoteUser.php
+++ app/Model/VoteUser.php
@@ -11,8 +11,4 @@ class VoteUser extends Model
     //模型日期列的存储格式
     protected $dateFormat = 'Y-m-d H:i:s';
 
-    public static function getList($where, $field, $page){
-        return self::field($field)->where($where)->limit($page)->get();
-    }
-
 }
