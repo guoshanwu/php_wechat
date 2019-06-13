774c721495fd767c7b492cce5127f159d8ba7eb6
linjianqun <11038667@qq.com>
2019-06-13 17:36:45 +0800

    ...

--- app/Http/Controllers/Api/BaseController.php
+++ app/Http/Controllers/Api/BaseController.php
@@ -11,10 +11,19 @@ class BaseController extends Controller
 
     public function __construct(){
         $this->middleware(function($request, $next){
-            $this->openid = session('wechat.oauth_user.default.id');
-            $userInfo = Users::find($this->openid);
-            dd($userInfo);
-            return $next($request);
+	    $this->openid = session('wechat.oauth_user.default.id');
+            $user = Users::find($this->openid);
+            if (empty($user)){
+		//第一次登陆,新增用户
+                $user = new Users();
+                $user->openid = $this->openid;
+            }else{
+		//已存在,更新登陆时间
+		$user->updated_at = date('Y-m-d H:i:s');
+	    }
+            if ($user->save()){
+                return $next($request);
+            }
         });
     }
 
