05f88ded9c98fbe3af75432a304a55e41d6ca329
linjianqun <11038667@qq.com>
2019-08-22 17:36:12 +0800

    ...

--- app/Http/Controllers/Api/Wechat.php
+++ app/Http/Controllers/Api/Wechat.php
@@ -8,8 +8,11 @@ class Wechat extends Base
      * @api {get} api/Banner/index  微信授权
      */
     public function index(){
-        $data = $this->request->input();
-        dd($data);
+        $redirecUrl = $this->request->input('redirect_url');
+        $appId = env('WECHAT_OFFICIAL_ACCOUNT_APPID');//获取自己公众号的 appid
+        $redirectUri = urlencode($redirecUrl);//处理url
+        $url = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=".$appId."&redirect_uri=".$redirectUri."&response_type=code&scope=snsapi_userinfo&state=1#wechat_redirect";
+        header('location:'.$url);
     }
 
 }
