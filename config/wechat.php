979bdf43767f6fd95cc6a813a2ba826147af379d
linjianqun <11038667@qq.com>
2019-08-22 10:57:01 +0800

    ...

--- config/wechat.php
+++ config/wechat.php
@@ -72,7 +72,7 @@ return [
              */
              'oauth' => [
 //                 'scopes'   => array_map('trim', explode(',', env('WECHAT_OFFICIAL_ACCOUNT_OAUTH_SCOPES', 'snsapi_userinfo'))),
-                 'callback' => env('WECHAT_OFFICIAL_ACCOUNT_OAUTH_CALLBACK', 'web.tenstudio.top'),
+                 'callback' => env('WECHAT_OFFICIAL_ACCOUNT_OAUTH_CALLBACK', 'http://web.tenstudio.top'),
              ],
         ],
     ],
