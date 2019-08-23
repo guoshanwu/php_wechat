6a55a1a009c277fc5191acbf4b48a717a6ca1715
linjianqun <11038667@qq.com>
2019-08-23 12:48:12 +0800

    ...

---
+++ app/Http/Controllers/Api/Index.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+
+class Index extends Controller
+{
+    public function index(){
+        return view('web.index');
+    }
+
+}
