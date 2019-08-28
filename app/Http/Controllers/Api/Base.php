325e0432adc45aa8e82133b595d6085d4281adbb
linjianqun <11038667@qq.com>
2019-08-28 17:51:34 +0800

    ...

--- app/Http/Controllers/Api/Base.php
+++ app/Http/Controllers/Api/Base.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Api;
 use App\Model\Image;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use Illuminate\Support\Facades\Session;
 
 class Base extends Controller
 {
